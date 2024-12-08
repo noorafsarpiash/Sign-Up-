196968bc351bb81e4c78b91b9745c654832e9838
noorafsarpiash <noorpiash1234@gmail.com>
2024-12-08 13:07:06 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+import LoginSignUp from "./components/loginSignUp/LoginSignup";
+
+function App() {
+  useEffect;
+  useState;
+  return (
+    <>
+      <LoginSignUp />
+    </>
+  );
+}
+
+export default App;
